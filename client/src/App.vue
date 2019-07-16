7ff67753fadd40c9256be3b88d29d8fcfdacde9b
navix <d1sruptive@protonmail.com>
2019-07-16 14:41:40 +0200

    :tada: Initial Commit!

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <AppNav/>
+    <router-view class="container"/>
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav,
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community!</h1>
+      <p class="lead">A fully open-source forum. Made with ❤️</p>
+      <hr class="my-4">
+      <p>This forum is all about community. Be kind! Be helpful! 🌈</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
